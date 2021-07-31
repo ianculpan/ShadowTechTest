8f9206501a1458ccd2d8b725f7b9c15dd7a6c7b8
vagrant <vagrant@vagrant.vm>
2021-07-31 14:53:04 +0000

    Meta data import started with first test passed

--- app/Console/Commands/ImportBooksMeta.php
+++ app/Console/Commands/ImportBooksMeta.php
@@ -11,14 +11,23 @@ class ImportBooksMeta extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'import:book-metadata {--f|file= : CSV file name}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Import book meta data from csv';
+
+    /**
+     * @var string
+     */
+    protected $fileName = "";
+    /**
+     * @var bool
+     */
+    private $skipFirstLine;
 
     /**
      * Create a new command instance.
@@ -37,6 +46,30 @@ class ImportBooksMeta extends Command
      */
     public function handle()
     {
+        $this->getFileName();
         return 0;
     }
+
+    /**
+     * @return string
+     */
+    public function getFileName(): string
+    {
+        if (!$this->option('file')) {
+            $this->error('You need to supply a file name.');
+            throw new \Symfony\Component\Console\Exception\RuntimeException;
+        } else {
+            $this->setFileName($this->option('file'));
+        }
+        return $this->fileName;
+    }
+
+    /**
+     * @param string $fileName
+     */
+    public function setFileName(string $fileName): void
+    {
+        $this->fileName = $fileName;
+    }
+
 }
